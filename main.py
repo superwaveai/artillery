8dee4824d39ee46a312d180856c8de99da8d2a4b
pranavs1080 <pranavs1080@gmail.com>
2023-11-26 03:54:07 +0530

    added websocket

--- main.py
+++ main.py
@@ -4,42 +4,47 @@ from azure.communication.email import EmailClient, EmailContent, EmailMessage, E
 from azure.core.exceptions import ServiceRequestError
 import csv
 import io
+from flask_socketio import SocketIO
 
 app = Flask(__name__)
+socketio = SocketIO(app)
 
+email_logs = []
 
-@app.route('/')
+@app.route('/', methods=['GET','POST'])
 def index():
-    return render_template('mailer.html')
+    if request.method == 'POST':
+        sender_address = request.form['sender_address']
+        subject_template = request.form['subject_template']
+        reply_to_address = request.form['reply_to_address']
+        num_threads = int(request.form['num_threads'])
+        html_content = request.files['html_content']
+        connection_string = request.form['connection_string']
+        csv_file = request.files['csv_file']
 
+        contacts = read_csv(csv_file)
+        email_client = EmailClient.from_connection_string(connection_string)
 
-@app.route('/submit', methods=['POST'])
-def submit():
-    sender_address = request.form['sender_address']
-    subject_template = request.form['subject_template']
-    reply_to_address = request.form['reply_to_address']
-    num_threads = int(request.form['num_threads'])
-    html_content = request.form['html_content']
-    connection_string = request.form['connection_string']
-    csv_file = request.files['csv_file']
+        # Create and start a separate thread for sending emails.
+        threads = []
+        for contact in contacts:
+            if threading.active_count() < num_threads:
+                thread = threading.Thread(target=send_email, args=(
+                email_client, contact, sender_address, subject_template, reply_to_address, html_content))
+                threads.append(thread)
+                thread.start()
 
-    contacts = read_csv(csv_file)
-    email_client = EmailClient.from_connection_string(connection_string)
+        for thread in threads:
+            thread.join()
 
-    # Create and start a separate thread for sending emails.
-    threads = []
-    for contact in contacts:
-        if threading.active_count() < num_threads:
-            thread = threading.Thread(target=send_email, args=(
-            email_client, contact, sender_address, subject_template, reply_to_address, html_content))
-            threads.append(thread)
-            thread.start()
+    return render_template('mailer.html',email_logs=email_logs)
 
-    for thread in threads:
-        thread.join()
+@socketio.on('connect')
+def handle_connect():
+    emit_logs()
 
-    return jsonify({'message': 'Emails are being sent.'})
-
+def emit_logs():
+    socketio.emit('update_logs', {'logs': email_logs}, namespace='/')
 
 def read_csv(csv_file):
     stream = io.StringIO(csv_file.stream.read().decode("UTF8"), newline=None)
@@ -69,11 +74,22 @@ def send_email(email_client, contact, sender_address, subject_template, reply_to
         # Replace 'begin_send' with the appropriate method to send an email
         email_client.send(email_message)
 
-        print(f"Email sent to {name} at {email}")
+        # print(f"Email sent to {name} at {email}")
+        log_message = f"Email sent to {name} at {email}"
+        email_logs.append(log_message)
+        print(log_message)
+
+        # Emit the updated logs to connected clients
+        emit_logs()
 
     except ServiceRequestError as e:
-        print(f"Failed to send email to {email}: {e}")
+        # print(f"Failed to send email to {email}: {e}")
+        error_message = f"Failed to send email to {email}: {e}"
+        email_logs.append(error_message)
+        print(error_message)
 
+        # Emit the updated logs to connected clients
+        emit_logs()
 
 if __name__ == '__main__':
     app.run(debug=True)
